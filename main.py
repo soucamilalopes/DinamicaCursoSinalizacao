e775598e8807980c04d9505d0ba9dded8f2f0603
Camila Lopes <46307429-camilanh3@users.noreply.replit.com>
2025-08-12 10:14:33 +0000

    versao1

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 #
 #Sinalizacao
-###
+#
 
 f = open("sinalizacao.csv")
 
-for line in f:
-  print(line.strip().split(';'))
 
+antiga = ''
+primeira = True
+ausentes = []
+for line in f:
+  if primeira:
+    primeira = False
+    continue
+  lista = line.strip().split(';')
+  if antiga == '' or antiga > lista[4]:
+   antiga = lista[4]
+  if lista[13].strip() == '' or lista[14].strip() == '':
+    ausentes.append(lista)
+  else:
+    print(lista)
+  
+  
+print(antiga)
+print(ausentes)
