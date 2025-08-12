007f1e4f133f73367fde8dbafee79bac5edde83c
Camila Lopes <46307429-camilanh3@users.noreply.replit.com>
2025-08-12 10:48:08 +0000

    Criação da classe

--- main.py
+++ main.py
@@ -2,24 +2,32 @@
 #Sinalizacao
 #
 
-f = open("sinalizacao.csv")
+class Sign:
+  def __init__(self):
+    self.f = open("sinalizacao.csv")
 
+  def Extrair(self):
+    f = self.f
+    antiga = ''
+    primeira = True
+    ausentes = []
+    for line in f:
+      if primeira:
+        primeira = False
+        continue
+      lista = line.strip().split(';')
+      if antiga == '' or antiga > lista[4]:
+        antiga = lista[4]
+      if lista[13].strip() == '' or lista[14].strip() == '':
+        ausentes.append(lista)
+      else:
+       print(lista)
 
-antiga = ''
-primeira = True
-ausentes = []
-for line in f:
-  if primeira:
-    primeira = False
-    continue
-  lista = line.strip().split(';')
-  if antiga == '' or antiga > lista[4]:
-   antiga = lista[4]
-  if lista[13].strip() == '' or lista[14].strip() == '':
-    ausentes.append(lista)
-  else:
-    print(lista)
-  
-  
-print(antiga)
-print(ausentes)
+    return antiga, ausentes
+
+sign = Sign()
+antiga = sign.Extrair()
+ausentes = sign.Extrair()
+
+print("Antiga:", antiga)
+print("Ausentes:", ausentes)
